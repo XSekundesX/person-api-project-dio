554aa183dd39bfc02cb03ea4eaca446f94b4c53d
XSekundesX <guga_silva@live.com>
2021-07-10 14:26:28 -0300

    Inicial commit: Person API Project for DIO's Bootcamp

---
+++ src/main/java/one/innovation/digital/personapiproject/PersonapiprojectApplication.java
@@ -0,0 +1,13 @@
+package one.innovation.digital.personapiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/innovation/digital/personapiproject/PersonapiprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package one.innovation.digital.personapiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
